b3693b35d5273256021844afcaf3b0f18058e41c
Gabriel Falcão <joaodamaceno@cers.com.br>
2023-05-17 20:15:39 -0300

    fix names properties talbes insert

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO user(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO user(name, email, password) VALUES('Aluno', 'aluno@email.com', '123456');
 
-INSERT INTO course(nome, categoria) VALUES('Spring Boot', 'Programação');
-INSERT INTO course(nome, categoria) VALUES('HTML 5', 'Front-end');
+INSERT INTO course(name, category) VALUES('Spring Boot', 'Programação');
+INSERT INTO course(name, category) VALUES('HTML 5', 'Front-end');
 
-INSERT INTO topic(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida', 'Erro ao criar projeto', '2019-05-05 18:00:00', 'NAO_RESPONDIDO', 1, 1);
-INSERT INTO topic(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 2', 'Projeto não compila', '2019-05-05 19:00:00', 'NAO_RESPONDIDO', 1, 1);
-INSERT INTO topic(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 3', 'Tag HTML', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
+INSERT INTO topic(title, message, created_at, status, author_id, course_id) VALUES('Dúvida', 'Erro ao criar projeto', '2019-05-05 18:00:00', 'NAO_RESPONDIDO', 1, 1);
+INSERT INTO topic(title, message, created_at, status, author_id, course_id) VALUES('Dúvida 2', 'Projeto não compila', '2019-05-05 19:00:00', 'NAO_RESPONDIDO', 1, 1);
+INSERT INTO topic(title, message, created_at, status, author_id, course_id) VALUES('Dúvida 3', 'Tag HTML', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
